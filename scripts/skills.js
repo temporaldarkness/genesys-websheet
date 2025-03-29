ab95b145a75e55b89cb54b2e3fa024ee183562d4
temporaldarkness <94239354+temporaldarkness@users.noreply.github.com>
2025-03-29 22:44:25 +0300

    Доработка хранения навыков
    Навыки хранятся в более удобном виде для дальнейших изменений.

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,80 +1,81 @@
-function generateHTML(data) 
+function generateHTML(data, categories) 
 {
 	const container = document.querySelector('.skills-container');
 	container.innerHTML = '';
 	
-	data.forEach(data => {
-		if (data.display)
-		{
-			category = document.createElement('div');
-			category.classList.add('skill-category');
-			
-			header = document.createElement('div');
-			header.classList.add('skill-header', 'bottomline');
-			
-			header.innerHTML = `
-				<span>${data.category}</span>
-				<span>Карьерный?</span>
-				<span>Ранг</span>
+	const sortedSkills = data.reduce((acc, skill) => {
+	    const categoryId = skill['category'];
+	    if (!acc[categoryId])
+	        acc[categoryId] = [];
+	    
+	    if (skill.display)
+	        acc[categoryId].push(skill);
+	    
+	    return acc;
+	}, {});
+	
+	Object.entries(sortedSkills).forEach(([categoryId, skills]) => {
+	    if (skills.length === 0)
+	        return;
+	    category = document.createElement('div');
+		category.classList.add('skill-category');
+		
+		header = document.createElement('div');
+		header.classList.add('skill-header', 'bottomline');
+		header.innerHTML = `
+			<span>${categories[categoryId]}</span>
+			<span>Карьерный?</span>
+			<span>Ранг</span>
+		`;
+		category.appendChild(header);
+		
+		grid = document.createElement('div');
+		grid.classList.add('skills-grid');
+		
+    	skills.forEach(skill => {
+    	    item = document.createElement('div');
+			item.classList.add('skill-item');
+			item.innerHTML = `
+				<span class="skill-name">${skill.name} (${skill.attribute})</span>
+				<input type="checkbox">
 			`;
-			category.appendChild(header);
 			
-			grid = document.createElement('div');
-			grid.classList.add('skills-grid');
+			selector = document.createElement('div');
+			selector.classList.add('rank-selector');
+			selector.dataset.rank = '0';
+			selector.innerHTML = `
+				<div class="rank-dot" data-value="0"></div>
+				<div class="rank-dot" data-value="1"></div>
+				<div class="rank-dot" data-value="2"></div>
+				<div class="rank-dot" data-value="3"></div>
+				<div class="rank-dot" data-value="4"></div>
+			`;
+			item.appendChild(selector);
+			grid.appendChild(item);
 			
-			data.skills.forEach(skill => {
-				if (skill.display)
-				{
-					const item = document.createElement('div');
-					item.classList.add('skill-item');
-					
-					item.innerHTML = `
-						<span class="skill-name">${skill.name} (${skill.attribute})</span>
-						<input type="checkbox">
-					`;
-					
-					selector = document.createElement('div');
-					selector.classList.add('rank-selector');
-					selector.dataset.rank = '0';
-					selector.innerHTML = `
-						<div class="rank-dot" data-value="0"></div>
-						<div class="rank-dot" data-value="1"></div>
-						<div class="rank-dot" data-value="2"></div>
-						<div class="rank-dot" data-value="3"></div>
-						<div class="rank-dot" data-value="4"></div>
-					`
-					item.appendChild(selector);
-					grid.appendChild(item);
-					
-					const dots = selector.querySelectorAll('.rank-dot');
-					
-					selector.addEventListener('click', function(e) {
-						const dot = e.target.closest('.rank-dot');
-						if (!dot)
-							return;
-						
-						currentRank = parseInt(dot.dataset.value) + 1;
-						if (e.currentTarget.dataset.rank == currentRank)
-							currentRank = 0;
-						
-						dots.forEach((d, index) => {
-						const isActive = index < currentRank;
-							d.classList.toggle('active', isActive);
-						});
-						e.currentTarget.dataset.rank = currentRank;
-					});
-					
-					selector.addEventListener('update', function(e) {
-            			dots.forEach((d, index) => {
-            			const isActive = index < e.currentTarget.dataset.rank;
-            				d.classList.toggle('active', isActive);
-            			});
-            		});
-				}
+			const dots = selector.querySelectorAll('.rank-dot');
+			selector.addEventListener('click', function(e) {
+			    const dot = e.target.closest('.rank-dot');
+				if (!dot) return;
+				currentRank = parseInt(dot.dataset.value) + 1;
+				if (parseInt(this.dataset.rank) === currentRank)
+					currentRank = 0;
+				
+				dots.forEach((d, index) => {
+					d.classList.toggle('active', index < currentRank);
+				});
+				this.dataset.rank = currentRank;
 			});
-			category.appendChild(grid);
-			container.appendChild(category);
-		}
+			
+			selector.addEventListener('update', function(e) {
+            	dots.forEach((d, index) => {
+            	const isActive = index < e.currentTarget.dataset.rank;
+            		d.classList.toggle('active', isActive);
+            	});
+            });
+		});
+		category.appendChild(grid);
+		container.appendChild(category);
 	});
 }
 
@@ -86,7 +87,7 @@ async function loadSkillsData()
             throw new Error(`Ошибка загрузки данных: ${response.status}`);
 		
         const data = await response.json();
-        generateHTML(data);
+        generateHTML(data['skills'], data['categories']);
     } catch (error) {
         console.error('Ошибка:', error);
     }
