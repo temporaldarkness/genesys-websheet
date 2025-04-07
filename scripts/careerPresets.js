af611dbef9578397b5ba138f1a9e168f75c46dbe
temporaldarkness <94239354+temporaldarkness@users.noreply.github.com>
2025-04-08 00:45:07 +0300

    Карьеры
    Добавляет внутренний справочник по карьерам.

--- scripts/careerPresets.js
+++ scripts/careerPresets.js
@@ -1,5 +1,42 @@
 careerPresets = {};
+careerInfo = {};
 careerIds = [];
+
+function generateCareerHTML() 
+{
+	const container = document.getElementById('careers-container');
+	container.innerHTML = '';
+	
+	careerIds.forEach(presetId => {
+        let preset = careerPresets[presetId];
+        let infoPreset = careerInfo[presetId];
+        if (preset.display)
+        {
+    	    section = document.createElement('div');
+    	    section.classList.add('data-segment');
+    	    
+    	    list = '';
+    	    preset.values.forEach(val => {
+    	        list += `<li>${availableSkills.find(item => item.id == val).name}</li>\n`;
+    	    });
+    	    
+    	    
+    	    section.innerHTML = `
+    	        <h3 class="section-title">${preset.name}</h3>
+                <div class="block-subtitle">Описание</div>
+                <div class="section">${infoPreset.desc}</div>
+                <div class="block-subtitle">Карьерные навыки</div>
+                <div class="section">
+                    <ul>${list}</ul>
+                    <div class="wiki-note">
+                        Перед распределением очков опыта ${preset.name} может выбрать четыре разных карьерных навыка и повысить каждый из них на 1 ранг.
+                    </div>
+                </div>
+    	    `;
+    	    container.appendChild(section);
+        }
+	});
+}
 
 async function retrieveCareerPresets() 
 {
@@ -23,22 +60,49 @@ async function retrieveCareerPresets()
         console.error('Ошибка:', error);
     }
     select = document.getElementById('careerSelect');
-    careerIds.forEach(presetId => {
-       let preset = careerPresets[presetId];
-       if (preset.display)
-       {
-           let option = document.createElement('option');
-           
-           option.value = preset.id;
-           option.innerHTML = preset.name;
-           
-           select.appendChild(option);
-       }
-    });
+    if (select)
+    {
+        careerIds.forEach(presetId => {
+           let preset = careerPresets[presetId];
+           if (preset.display)
+           {
+               let option = document.createElement('option');
+               
+               option.value = preset.id;
+               option.innerHTML = preset.name;
+               
+               select.appendChild(option);
+           }
+        });
+        
+        select.addEventListener('change', function(){
+            setCareerPreset(parseInt(this.value));
+        });
+    }
     
-    select.addEventListener('change', function(){
-        setCareerPreset(parseInt(this.value));
-    });
+    if (document.getElementById('careers-container'))
+        retrieveCareerInfo();
+}
+
+async function retrieveCareerInfo() 
+{
+    try {
+        const response = await fetch('./data/careers.json');
+        if (!response.ok)
+            throw new Error(`Ошибка загрузки пресетов: ${response.status}`);
+		
+        const data = await response.json();
+        data.forEach(preset => {
+           careerInfo[preset.id] = {
+               'desc': preset.desc
+            };
+        });
+        
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+    
+    generateCareerHTML();
 }
 
 function loadCareerPreset(preset)
